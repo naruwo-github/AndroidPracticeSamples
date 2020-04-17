154ddea82e777dbbe8c4777f7ad1701533300f88
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-17 18:10:17 +0900

    スピナー追加の準備

---
+++ SampleBodySize/app/src/main/java/com/example/samplebodysize/HeightActivity.kt
@@ -0,0 +1,14 @@
+package com.example.samplebodysize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HeightActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_height)
+
+        
+    }
+}

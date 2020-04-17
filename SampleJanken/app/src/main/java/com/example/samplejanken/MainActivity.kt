8eab1e0ea20fb8e1f2709097b0ed37a46fa20463
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-17 12:33:51 +0900

    画面遷移の追加(まだ戻れない)

--- SampleJanken/app/src/main/java/com/example/samplejanken/MainActivity.kt
+++ SampleJanken/app/src/main/java/com/example/samplejanken/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.samplejanken
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gu.setOnClickListener { onJankenButtonTapped(it) }
+        choki.setOnClickListener { onJankenButtonTapped(it) }
+        pa.setOnClickListener { onJankenButtonTapped(it) }
+    }
+
+    fun onJankenButtonTapped(view: View?) {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("My_HAND", view?.id)
+        startActivity(intent)
     }
 }

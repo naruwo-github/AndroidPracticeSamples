e33bc667a551a935af395b0cb33fad84ac91c967
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-17 18:32:50 +0900

    アクティビティとスピナーの追加

--- SampleBodySize/app/src/main/java/com/example/samplebodysize/HeightActivity.kt
+++ SampleBodySize/app/src/main/java/com/example/samplebodysize/HeightActivity.kt
@@ -2,6 +2,10 @@ package com.example.samplebodysize
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_height.*
 
 class HeightActivity : AppCompatActivity() {
 
@@ -9,6 +13,23 @@ class HeightActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_height)
 
-        
+        spinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val spinner = parent as? Spinner
+                    val item = spinner?.selectedItem as? String
+                    item?.let {
+                        if (it.isNotEmpty()) height.text = it
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
     }
 }

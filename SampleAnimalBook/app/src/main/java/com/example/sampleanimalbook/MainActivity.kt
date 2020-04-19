65baad78b886e70969a91a68eb3b598c2632b88e
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-19 10:12:34 +0900

    処理の追記

--- SampleAnimalBook/app/src/main/java/com/example/sampleanimalbook/MainActivity.kt
+++ SampleAnimalBook/app/src/main/java/com/example/sampleanimalbook/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.sampleanimalbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_sub.*
+import kotlinx.android.synthetic.main.activity_sub.titleFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,10 @@ class MainActivity : AppCompatActivity() {
 
         val fragment = titleFragment as? TitleFragment
         fragment?.setTitle("フラグメント動物図鑑")
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this, subActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

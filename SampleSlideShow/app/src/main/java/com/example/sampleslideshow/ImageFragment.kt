df91252b111399fa74984a56c6e90905767d41a3
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-21 09:54:39 +0900

    スライドショーアプリの完成

--- SampleSlideShow/app/src/main/java/com/example/sampleslideshow/ImageFragment.kt
+++ SampleSlideShow/app/src/main/java/com/example/sampleslideshow/ImageFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowId
+import kotlinx.android.synthetic.main.fragment_image.*
 
 /**
  * A simple [Fragment] subclass.
@@ -22,13 +23,32 @@ class ImageFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_image, container, false)
     }
 
+    //このクラスのインスタンスがまだ作成されてなくても、コンパニオンオブジェクトは呼べる
     companion object {
+        //画像のフラグメントのイニシャライズ処理と、アーギュメンツへの保存
         fun newInstance(imageResourceId: Int) : ImageFragment {
             val bundle = Bundle()
             bundle.putInt(IMG_RES_ID, imageResourceId)
             val imageFragment = ImageFragment()
             imageFragment.arguments = bundle
             return imageFragment
+        }
+    }
+
+    private var imgResId: Int? = null
+
+    //保存したアーギュメンツを取得し、画面を作成する
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            imgResId = it.getInt(IMG_RES_ID)
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        imgResId?.let {
+            imageView.setImageResource(it)
         }
     }
 

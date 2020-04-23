38b257f4fa97213fdb34520b9213f6b5f1dcb5d5
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-23 17:10:54 +0900

    メニューを選んで背景変更

--- SampleMenuApp/app/src/main/java/com/example/samplemenuapp/MainActivity.kt
+++ SampleMenuApp/app/src/main/java/com/example/samplemenuapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.samplemenuapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +19,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            R.id.top -> {
+                webView.loadUrl("file:///android_asset/html/index.html")
+                return true
+            }
+            R.id.lunch01 -> {
+                webView.loadUrl("file:///android_asset/html/lunch01.html")
+                return true
+            }
+            R.id.lunch02 -> {
+                webView.loadUrl("file:///android_asset/html/lunch02.html")
+                return true
+            }
+            R.id.dinner01 -> {
+                webView.loadUrl("file:///android_asset/html/dinner01.html")
+                return true
+            }
+            R.id.dinner02 -> {
+                webView.loadUrl("file:///android_asset/html/dinner02.html")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

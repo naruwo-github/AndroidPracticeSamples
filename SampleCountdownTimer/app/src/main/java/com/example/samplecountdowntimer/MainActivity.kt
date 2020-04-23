d226356b01a2c7d7f894411a1662462ddb9dae04
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-23 15:30:50 +0900

    動くようになった

--- SampleCountdownTimer/app/src/main/java/com/example/samplecountdowntimer/MainActivity.kt
+++ SampleCountdownTimer/app/src/main/java/com/example/samplecountdowntimer/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.example.samplecountdowntimer
 
+import android.icu.util.DateInterval
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    inner class MyCountDownTimer(millisInFuture: Long,
+    countDownInterval: Long) :
+    CountDownTimer(millisInFuture, countDownInterval) {
+        var isRunning = false
+
+        override fun onFinish() {
+            timerText.text = "0:00"
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+            val minute = millisUntilFinished / 1000L / 60L
+            val second = millisUntilFinished / 1000L % 60L
+            timerText.text = "%1d:%2$02d".format(minute, second)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        timerText.text = "3:00"
+        val timer = MyCountDownTimer(3 * 60 * 1000, 100)
+        playStop.setOnClickListener {
+            timer.isRunning = when (timer.isRunning) {
+                true -> {
+                    timer.cancel()
+                    playStop.setImageResource(
+                        R.drawable.ic_play_arrow_black_24dp
+                    )
+                    false
+                }
+                false -> {
+                    timer.start()
+                    playStop.setImageResource(
+                        R.drawable.ic_stop_black_24dp
+                    )
+                    true
+                }
+            }
+        }
     }
 }

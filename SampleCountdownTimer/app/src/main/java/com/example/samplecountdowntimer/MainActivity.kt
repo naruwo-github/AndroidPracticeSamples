7b3696e901fe460c945d0d123d88b7423270b5c3
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-23 15:52:01 +0900

    音楽つけて完成！

--- SampleCountdownTimer/app/src/main/java/com/example/samplecountdowntimer/MainActivity.kt
+++ SampleCountdownTimer/app/src/main/java/com/example/samplecountdowntimer/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.samplecountdowntimer
 
 import android.icu.util.DateInterval
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.ForkJoinPool
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var soundPool: SoundPool
+    private var soundResId = 0
 
     inner class MyCountDownTimer(millisInFuture: Long,
     countDownInterval: Long) :
@@ -15,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         override fun onFinish() {
             timerText.text = "0:00"
+            soundPool.play(soundResId, 1.0f, 100f, 0, 0, 1.0f)
         }
 
         override fun onTick(millisUntilFinished: Long) {
@@ -28,8 +37,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        timerText.text = "3:00"
-        val timer = MyCountDownTimer(3 * 60 * 1000, 100)
+        timerText.text = "1:00"
+        val timer = MyCountDownTimer(1 * 60 * 1000, 100)
         playStop.setOnClickListener {
             timer.isRunning = when (timer.isRunning) {
                 true -> {
@@ -48,5 +57,29 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        soundPool =
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                @Suppress("DEPRECATION")
+                SoundPool(2, AudioManager.STREAM_ALARM, 0)
+            } else {
+                val audioAttributes = AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+                SoundPool.Builder()
+                    .setMaxStreams(1)
+                    .setAudioAttributes(audioAttributes)
+                    .build()
+            }
+//        soundPool = SoundPool(2, AudioManager.STREAM_ALARM, 0)
+        soundResId = soundPool.load(this, R.raw.bellsound, 1)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        soundPool.release()
     }
 }

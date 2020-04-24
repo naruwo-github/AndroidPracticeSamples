3af4a520d26e9618f62d5faa8c5126494c667e27
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-25 07:49:32 +0900

    少し書いた

--- SampleScheduler/app/src/main/java/com/example/samplescheduler/MainActivity.kt
+++ SampleScheduler/app/src/main/java/com/example/samplescheduler/MainActivity.kt
@@ -5,20 +5,28 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import io.realm.Realm
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        realm = Realm.getDefaultInstance()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {

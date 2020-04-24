03aba51732ec40bb16f33685fcdcc7a5c5b48c67
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-24 20:37:00 +0900

    DatePickerDialog実装

--- SampleAlarmClock/app/src/main/java/com/example/samplealarmclock/Dialogs.kt
+++ SampleAlarmClock/app/src/main/java/com/example/samplealarmclock/Dialogs.kt
@@ -1,10 +1,13 @@
 package com.example.samplealarmclock
 
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
+import java.util.*
 
 class TimeAlertDialog : DialogFragment() {
     interface Listener {
@@ -33,4 +36,32 @@ class TimeAlertDialog : DialogFragment() {
         }
         return builder.create()
     }
+}
+
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+    interface OnDateSelectedListener {
+        fun onSelected(year: Int, month: Int, date: Int)
+    }
+
+    private var listener: OnDateSelectedListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        when (context) {
+            is OnDateSelectedListener -> listener = context
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val date = c.get(Calendar.DAY_OF_MONTH)
+        return DatePickerDialog(requireActivity(), this, year, month, date)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        listener?.onSelected(year, month, dayOfMonth)
+    }
+
 }
